04ec1779db26076a7fee8bec5e65f97c8c412b87
khermalik <khermalik@gmail.com>
2020-09-23 16:03:39 -0700

    wrote pseudo code and defined variables

---
+++ scripts/app.js
@@ -0,0 +1,32 @@
+// Create a class for Tralfamagotchi
+// Instantiate Salo
+// Create a timer that counts down from 10
+    // - attach the countdown to 'Sustenance', 'Energy', and 'Engagement'
+    // - will decrement scores at matching intervals.
+// Create another timer that counts up to 3 over 30 second intervals.
+    // - will increment 'age'.
+// Attach event listener to 'play' button 
+    // - will activate timer function
+// Attach event listeners to 'lights', 'food', 'engage'
+    // - will add 1 to 'Sustenance', 'Energy', and 'Engagement' (corresponding button)
+// Attach event listener to 'Rename' button
+    // - will activate form
+      // - grab input value of form and update 'name'
+// Create Game Over alert if any value reaches 0 or age reaches 3.
+
+
+
+
+
+
+
+let sustenance = 10;
+let energy = 10;
+let engagement = 10;
+
+let name = Salo;
+let age = 0;
+
+
+
+
