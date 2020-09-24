3044650cd84a6542c19a2d8d04720fa845eb4add
khermalik <khermalik@gmail.com>
2020-09-23 19:16:11 -0700

    Created age timer

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,12 @@
 // Create a class for Tralfamagotchi
 // Instantiate Salo
+// Attach event listener to 'play' button 
+    // - will activate timer function
 // Create a timer that counts down from 10
     // - attach the countdown to 'Sustenance', 'Energy', and 'Engagement'
     // - will decrement scores at matching intervals.
 // Create another timer that counts up to 3 over 30 second intervals.
     // - will increment 'age'.
-// Attach event listener to 'play' button 
-    // - will activate timer function
 // Attach event listeners to 'lights', 'food', 'engage'
     // - will add 1 to 'Sustenance', 'Energy', and 'Engagement' (corresponding button)
 // Attach event listener to 'Rename' button
@@ -24,11 +24,25 @@ class Pet {
     this.engagement = engagement;
   }
 
+  startAgeTimer () {
+    let self = this;
+    const timerId = setInterval( function() {
+      if (self.age === 2) {
+        alert('GAME OVER - So it goes.');
+        clearInterval(timerId);
+      }
+        self.age++;
+        $('#age').text(`Age: ${self.age}`);
+    }, 5000);
+  }
+
+
 }
 
 class Tralfamagotchi extends Pet {
   constructor(name) {
     super(name, 0, 10, 10, 10);
+    $('#name').text(`Name: ${name}`)
   }
 }
 
@@ -36,7 +50,8 @@ const Salo = new Tralfamagotchi('Salo');
 
 
 $('#play').on('click', () => {
-
-
+  Salo.startAgeTimer();
 });
 
+
+console.log(Salo.age)
